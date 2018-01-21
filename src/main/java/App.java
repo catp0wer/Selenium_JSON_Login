a2bbdf6ac5c375c9ae348a312887df252507a5da
laila <laila@fake.com>
2018-01-21 22:41:16 +0200

    compare the titlePage from json with the titlePage received, also added in json another atribbute - titlePage

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,10 @@
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-//import net.sf.json.JSONArray;
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Iterator;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -20,40 +15,37 @@ public class App {
 
         JSONParser parser = new JSONParser();
         try {
-            Object obj = parser.parse(new FileReader("D:\\Git_projects\\Selenium_JSON_Login\\src\\main\\java\\credentials.json"));
+            Object obj = parser.parse(new FileReader("D:\\experiments\\java_projects\\Selenium_JSON_Login\\src\\credentials.json"));
             JSONObject jsonObject = (JSONObject) obj;
-           // jsonObject.
-           JSONArray cases =(JSONArray) jsonObject.get("Testcases");
+            JSONArray cases =(JSONArray) jsonObject.get("Testcases");
 
-           System.out.printf("Successfully read %d cases\n", cases.size());
-           for(int i=0; i<cases.size();i++){
-               JSONObject testcase = (JSONObject) cases.get(i);
-               String email = testcase.get("email").toString();
-               String password = (String) testcase.get("password");
-               System.out.printf("Testcase %d: email %s\t\t pass %s\n", i,email,password);
+            System.out.printf("Successfully read %d cases\n", cases.size());
+            for(int i=0; i<cases.size();i++){
+                JSONObject testcase = (JSONObject) cases.get(i);
+                String email = testcase.get("email").toString();
+                String password = (String) testcase.get("password");
+                String pageTitle = (String) testcase.get("pageTitle");
+                System.out.printf("Testcase %d: email %s\t\t pass %s\n", i,email,password);
 
+                WebDriver driver = new FirefoxDriver();
+                driver.get("http://www.phptravels.net/login");
+                driver.findElement(By.name("username")).sendKeys(email);
+                driver.findElement(By.name("password")).sendKeys(password);
+                Thread.sleep(1000);
+                driver.findElement(By.xpath(".//*[@id='loginfrm']/div[1]/div[5]/button")).click();
+                Thread.sleep(1000);
 
-               WebDriver driver = new FirefoxDriver();
-               driver.get("http://www.phptravels.net/login");
-               driver.findElement(By.name("username")).sendKeys(email);
-               driver.findElement(By.name("password")).sendKeys(password);
-               Thread.sleep(4000);
-               driver.close();
-               Thread.sleep(4000);
-           }
-  //          Iterator<JSONObject> testcaseiterator = cases.iterator();
-//            while(testcaseiterator.hasNext()){
-//                System.out.println(testcaseiterator);
-//                System.out.println("poloboku");
-//           }
+                String actualTitle =driver.getTitle();
+                System.out.println("Title is: "+actualTitle);
+                if(actualTitle.equals(pageTitle)){
+                    System.out.println("Test passed");
+                } else{
+                    System.out.println("Test failed");
+                }
+                Thread.sleep(9000);
+                driver.close();
 
-
-            //System.out.println("Name is: " +email);
-
-
-
-            //driver.findElement(By.name("btnK")).click();
-            //driver.close();
+            }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
