080e555e03bc1f235659104724ce15d231aa43dc
laila <laila@fake.com>
2018-02-25 17:08:28 +0200

    added method Login

--- src/main/java/pageObject/LogInPage.java
+++ src/main/java/pageObject/LogInPage.java
@@ -3,39 +3,41 @@ package pageObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-/**
- * Created by Liuda on 2/22/2018.
- */
 public class LogInPage {
+    public WebDriver driver;
 
-    private static WebElement element = null;
+//    public LogInPage(WebDriver driver){
+//        this.driver = driver;
+//   }
 
     public static WebElement userName_Field(WebDriver driver){
-        element = driver.findElement(By.name("username"));
-       return element;
+       return driver.findElement(By.name("username"));
     }
 
     public static WebElement password_Field(WebDriver driver){
-        element = driver.findElement(By.name("password"));
-        return element;
+        return driver.findElement(By.name("password"));
     }
     public static WebElement login_Button(WebDriver driver){
-        element = driver.findElement(By.xpath("//*[contains(@class, 'loginbtn')]"));
-        return element;
+        return driver.findElement(By.xpath("//*[contains(@class, 'btn btn-action btn-lg btn-block loginbtn')]"));
+    }
+    public static WebElement invalidEmailOrPassword(WebDriver driver){
+        return driver.findElement(By.xpath("//*[contains(@class, 'alert alert-danger')]"));
     }
 
-}
+    public static void Login(String email, String password,WebDriver driver){
 
-//
-//public class LogIn_Page {
-//
-//    private static WebElement element = null;
-//
-//    public static WebElement txtbx_UserName(WebDriver driver){
-//
-//        element = driver.findElement(By.id("log"));
-//
-//        return element;
-//
-//    }
+        LogInPage.userName_Field(driver).sendKeys(email);
+        LogInPage.password_Field(driver).sendKeys(password);
+        //added sleep to wait while Login page is clickable
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        LogInPage.login_Button(driver).click();
+
+    }
+}
