7fc678c2976d411eae4176ab78acfdc34b438994
laila <laila@fake.com>
2018-02-25 17:05:31 +0200

    added functionality which determines what element to wait for in case of successfully or unsuccessfully login

--- src/main/java/LoginTest.java
+++ src/main/java/LoginTest.java
@@ -4,57 +4,45 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import pageObject.HomePage;
 import pageObject.LogInPage;
 
 public class LoginTest {
 
     boolean result;
 
-    public boolean test(TestCase[] credentialArray){
-        for(int i=0;i<credentialArray.length;i++){
+    public boolean test(TestCase[] credentialArray) {
+
+        for (int i = 0; i < credentialArray.length; i++) {
             WebDriver driver = new FirefoxDriver();
             driver.get("http://www.phptravels.net/login");
-            LogInPage.userName_Field(driver).sendKeys(credentialArray[i].email);
-           // driver.findElement(By.name("username")).sendKeys(credentialArray[i].email);
-            LogInPage.password_Field(driver).sendKeys(credentialArray[i].password);
-           // driver.findElement(By.name("password")).sendKeys(credentialArray[i].password);
-           // boolean present;
-            try {
-                Thread.sleep(1000);
-                LogInPage.login_Button(driver).click();
+            LogInPage.Login(credentialArray[i].email, credentialArray[i].password,driver);
 
-                Thread.sleep(2000);
-                WebDriverWait wait = new WebDriverWait(driver, 15);
-                wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='body-section']/div/div[1]/div/div[1]/h3")));
+            System.out.println("Before waiting");
 
-//                boolean present=driver.findElements(By.xpath("//*[@id='body-section']/div/div[1]/div/div[1]/h3")).size()!=0;
-//                       // (By.xpath(credentialArray[i].element)).size()!=0;
-//                        ///"[contains(text(),'Hi, DVhbCERv IlqEZZxz')]")).size()!=0;
-//               // String actualTitle = driver.getTitle();
-//               // System.out.println("Title is: " + actualTitle);
-//
-//               // result = actualTitle.equals(credentialArray[i].pageTitle);
-//                if (present) {
-//                    result = true;
-//                } else {
-//                    result = false;
-//                }
-
-                //just to see by myself the page
-                Thread.sleep(9000);
-                driver.close();
-                System.out.println("Test case nb " +i+ " is true");
-                return true;
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }catch (NotFoundException e){
-
-                System.out.println("Test case nb " +i+ " is false");
-               // Thread.sleep(9000);
-                driver.close();
-                return false;
+            String actualText = "";
+            if(credentialArray[i].expectedLogin.equals("Login_Successfully")) {
+                WebDriverWait wait = new WebDriverWait(driver, 1000);
+                wait.until(ExpectedConditions.visibilityOf(HomePage.welcome_el(driver)));
+                System.out.println("Before text element");
+                actualText = HomePage.welcome_el(driver).getText();
             }
-        }
-        return result;
+            else if(credentialArray[i].expectedLogin.equals("Login_Unsuccessfully")) {
+                WebDriverWait wait = new WebDriverWait(driver, 1000);
+                System.out.println("Unsuccessfully");
+                wait.until(ExpectedConditions.visibilityOf(LogInPage.invalidEmailOrPassword(driver)));
+                actualText = LogInPage.invalidEmailOrPassword(driver).getText();
+            }
+
+            if (actualText.equals(credentialArray[i].element_text)) {
+                result = true;
+            } else {
+                result = false;
+            }
+
+            driver.close();
+            System.out.println("Test case nb " + i + " is: " + result);
+
+        }return result;
     }
 }
