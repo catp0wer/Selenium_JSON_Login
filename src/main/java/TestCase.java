b5a216095f70d72389bbd90cab55532f56fe9155
laila <laila@fake.com>
2018-02-25 17:06:20 +0200

    added element_text and expectedLogin fields

--- src/main/java/TestCase.java
+++ src/main/java/TestCase.java
@@ -3,13 +3,14 @@ public class TestCase {
 
     String email;
     String password;
-    String element;
+    String element_text;
+    String expectedLogin;
 
-    public TestCase(String email, String password,String element){
+    public TestCase(String email, String password,String element_text,String expectedLogin){
         this.email = email;
         this.password = password;
-        this.element = element;
-
+        this.element_text = element_text;
+        this.expectedLogin = expectedLogin;
 
     }
 }
