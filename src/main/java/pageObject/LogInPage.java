3850377d312a91d238de5d4e00dcc3b952cc8feb
laila <laila@fake.com>
2018-03-25 22:18:18 +0300

    changed xpaths for the new site

--- src/main/java/pageObject/LogInPage.java
+++ src/main/java/pageObject/LogInPage.java
@@ -1,6 +1,7 @@
 package pageObject;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotInteractableException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -13,29 +14,41 @@ public class LogInPage {
 //   }
 
     public static WebElement userName_Field(WebDriver driver){
-       return driver.findElement(By.name("username"));
+
+        return driver.findElement(By.id("usernameOrEmail"));
     }
 
     public static WebElement password_Field(WebDriver driver){
-        return driver.findElement(By.name("password"));
+
+        return driver.findElement(By.id("password"));
     }
+    //button[@class='button form-button is-primary']
+
     public static WebElement login_Button(WebDriver driver){
-        return driver.findElement(By.xpath("//*[contains(@class, 'btn btn-action btn-lg btn-block loginbtn')]"));
+        return driver.findElement(By.xpath("//button[@class='button form-button is-primary']"));
     }
     public static WebElement invalidEmailOrPassword(WebDriver driver){
-        return driver.findElement(By.xpath("//*[contains(@class, 'alert alert-danger')]"));
+
+        return driver.findElement(By.xpath("//div[@class='form-input-validation is-error']"));
     }
 
     public static void Login(String email, String password,WebDriver driver){
 
         LogInPage.userName_Field(driver).sendKeys(email);
-        LogInPage.password_Field(driver).sendKeys(password);
-        //added sleep to wait while Login page is clickable
+        LogInPage.login_Button(driver).click();
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        try {
+            LogInPage.password_Field(driver).sendKeys(password);
+            //added sleep to wait while Login page is clickable
+        }catch(ElementNotInteractableException e){
+            System.out.println("Username is wrong.");
+            return;
+        }
+
 
         LogInPage.login_Button(driver).click();
 
