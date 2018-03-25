a855ea529962631326a1a01c8b64a16c05bd3acc
laila <laila@fake.com>
2018-03-25 22:06:37 +0300

    creating Logs object

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,25 +1,15 @@
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class App {
 
     public static void main(String[] args) {
+        Logs log = new Logs();
         JsonReader reader = new JsonReader();
         TestCase myArray[] = reader.read();
-       // System.out.println(myArray);
-        LoginTest logintest=new LoginTest();
+
+        LoginTest logintest=new LoginTest(log);
         boolean result = logintest.test(myArray);
         System.out.println(result);
-
-        Logs log = new Logs();
+        log.closeFile();
 
 
             }
