3dd20537dfe635d079468bb469a62e09fb27f80e
laila <laila@fake.com>
2018-03-26 20:48:15 +0300

    added a new package and moved this file there

---
+++ src/main/java/core/TestCase.java
@@ -0,0 +1,17 @@
+package core;
+
+public class TestCase {
+
+    String email;
+    String password;
+    String element_text;
+    String expectedLogin;
+
+    public TestCase(String email, String password,String element_text,String expectedLogin){
+        this.email = email;
+        this.password = password;
+        this.element_text = element_text;
+        this.expectedLogin = expectedLogin;
+
+    }
+}
