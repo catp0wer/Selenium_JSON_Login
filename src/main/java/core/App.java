2244d907ba1b6bc7d851b1fd36d4586b49821c28
laila <laila@fake.com>
2018-03-26 20:42:57 +0300

    added/demoved some log steps and added a new package core where this file is moved

---
+++ src/main/java/core/App.java
@@ -0,0 +1,17 @@
+package core;
+
+public class App {
+
+    public static void main(String[] args) {
+        Logs log = new Logs();
+        JsonReader reader = new JsonReader();
+        TestCase myArray[] = reader.read();
+
+        LoginTest logintest=new LoginTest(log);
+        boolean result = logintest.test(myArray);
+        System.out.println(result);
+        log.closeFile();
+            }
+
+
+}
